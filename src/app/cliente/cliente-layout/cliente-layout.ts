cc6534ec082a9c6f6576319c2c907c7361c78618
fatalbrux <henry.quispe@sistemas.edu.bo>
2026-06-28 22:47:16 -0400

    Generando los archivos para cliente

---
+++ src/app/cliente/cliente-layout/cliente-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-layout',
+  imports: [],
+  templateUrl: './cliente-layout.html',
+  styleUrl: './cliente-layout.css',
+})
+export class ClienteLayout {}
